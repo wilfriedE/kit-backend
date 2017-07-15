037c56526f8f19e062e5f4e0a75d5090b42aec16
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-07-15 16:03:08 -0700

    Remove confirmable and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   namespace :api do
     root to: 'status#index'
 
-    mount_devise_token_auth_for 'User', at: 'auth'
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   end
 end
