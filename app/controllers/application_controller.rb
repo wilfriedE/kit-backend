38e05701f3cc596d5d28bbf693b87f6e9e3eeb4e
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-07-15 17:18:19 -0700

    Adjust exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 end
