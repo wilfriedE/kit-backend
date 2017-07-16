d7dbb6874e1817ccdf0278781c33e74fc80c9d38
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-07-15 17:13:18 -0700

    skip authenticity check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token
 end
