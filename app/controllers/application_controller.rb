a2908b0fa5777b63656d127b85e1f2dbf7bb094e
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-07-16 01:04:47 -0700

    Allow community id in params for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
   helper_method :success_resp, :failure_resp, :api_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   def success_resp(message='Invalid request', obj={})
@@ -15,4 +16,12 @@ class ApplicationController < ActionController::Base
   def api_user
     return User.find(params[:user_id]) if params[:user_id]
   end
+
+  protected
+
+  def configure_permitted_parameters
+     permitted_parameters = devise_parameter_sanitizer.instance_values['permitted']
+     permitted_parameters[:sign_up] << :community_id
+     permitted_parameters[:account_update] << :community_id
+   end
 end
